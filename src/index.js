d63dc1ff8092dd56b75ae0feddefbb1c1999692e
Sloont <colinabel93@gmail.com>
2021-06-19 19:53:20 -0400

    we're officially at proof of concept. context is the 2dcontext to render the svgs as images with base64 src attributes and backgroundCTX is the 2dcontext to render the backgrounds of each orb. special thanks to @Justin on SO for the idea to load the images globally. We can finally display the class objects on the canvas!!!

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import './styles/index.css';
+///test
+import { svgIdArray } from './svgArray';
+import { convertSVG } from './convertSVG';
+///
 
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
+const backgroundCTX = canvas.getContext("2d");
 
 var window_height = window.innerHeight;
 var window_width = window.innerWidth;
@@ -11,21 +16,23 @@ canvas.height = 400;
 
 canvas.style.background = "#232a2e"
 
-const convertSVG = (svgid) => {
-    const svg = document.getElementById(svgid);
+//LETS MAKE THE OBJECT TO HOLD THE IMAGES WITH KEYS
+const imagesObject = {};
+const convertedSVGS = {}
+//the key has to be called with bracket notation here 
+//with dot notation assignment we get one key, svgId
+const emptyImages = svgIdArray.map(svgId => imagesObject[svgId] = new Image()); 
 
-    const xml = new XMLSerializer().serializeToString(svg);
+const iterateTheObject = Object.keys(imagesObject);
 
-    const svg64 = btoa(xml);
-    const b64Start = 'data:image/svg+xml;base64, ';
+const base64ImgArray = iterateTheObject.map(svgId => convertedSVGS[svgId] = convertSVG(svgId));
 
-    return b64Start + svg64;
-}
+//this correctly assigns the src
+for (let svgId of iterateTheObject) {
+    imagesObject[svgId].src = convertedSVGS[svgId];
+};
 
-const img1  = new Image();
-img1.src = convertSVG('javascript-icon');
-const img2 = new Image();
-img2.src = convertSVG('java-icon');
+/////////////////////////////////////////////////////
 
 class Orb {
     constructor(xpos, ypos, radius, speed, image) {
@@ -35,73 +42,43 @@ class Orb {
         this.speed = speed;
         this.image = image;
     }
-    draw(context) {
-            context.save();
-            context.beginPath();
-            context.arc(this.xpos, this.ypos, this.radius, 0, Math.PI * 2, false);
-            context.clip();
-            context.drawImage(this.image, (this.xpos - this.radius), (this.ypos - this.radius), 64, 64);
-            context.restore();
+    draw(context, backgroundCTX) {
+        backgroundCTX.beginPath();
+        backgroundCTX.fillStyle = "white";
+        backgroundCTX.arc(this.xpos, this.ypos, this.radius, 0, Math.PI * 2, false);
+        backgroundCTX.fill();
+        backgroundCTX.closePath();
+
+        context.save();
+        context.beginPath();
+        /*test*/context.strokeStyle = "white";
+        /*test*/context.lineWidth = 5;
+        context.arc(this.xpos, this.ypos, this.radius, 0, Math.PI * 2, false);
+        context.clip();
+        context.drawImage(this.image, (this.xpos - this.radius), (this.ypos - this.radius), 64, 64);
+        /*test*/context.stroke();
+        context.restore();
+
+            
+    }
+            
+}
+const orbCollection = [];
+
+for (let i = 0; i < svgIdArray.length; i++) {
+    let randomx = Math.random() * 500;
+    let randomy = Math.random() * 400;
+    orbCollection.push(new Orb(randomx, randomy, 32, 1, imagesObject[svgIdArray[i]]));
+    
+}
+
+
+window.onload = function() {
+    for (let orb of orbCollection) {
+        orb.draw(context, backgroundCTX);
     }
 }
 
-const myOrb = new Orb(150, 150, 30, 1, img1);
-const myOrb2 = new Orb(350, 150, 30, 1, img2);
-
-window.onload = function() {
-myOrb.draw(context);
-myOrb2.draw(context);
-}
-
-
-
-// class Orb {
-//     constructor(xpos, ypos, radius, speed, image) {
-//         this.xpos = xpos;
-//         this.ypos = ypos;
-//         this.radius = radius;
-//         this.speed = speed;
-//         this.imageSRC = convertSVG(image);
-//         this.image = new Image();
-        
-//     }
-
-
-//     draw(ctx) {
-//         let image = this.image;
-//         image.onload = function() {
-//             // context.save();
-//             ctx.beginPath();
-//             ctx.arc(this.xpos, this.ypos, this.radius, 0, Math.PI * 2, false);
-//             ctx.clip();
-//             ctx.drawImage(image, (this.xpos - this.radius), (this.ypos - this.radius), 64, 64);
-//             // context.restore();
-//             // context.closePath();
-//         }
-//         image.src = this.imageSRC;
-
-//     }
-//     update() {
-//         this.draw(context);
-//     }
-// }
-// const myOrb = new Orb(300, 300, 30, 1, 'javascript-icon');
-// const myOrb2 = new Orb(150, 150, 30, 1, 'java-icon');
-
-// const img  = new Image();
-
-// img.onload = function() {
-//     context.save();
-//     context.beginPath();
-//     context.arc(myOrb.xpos, myOrb.ypos, myOrb.radius, 0, Math.PI * 2, false);
-//     context.clip();
-//     context.drawImage(img, (myOrb.xpos-myOrb.radius-2), (myOrb.ypos-myOrb.radius-2), 64, 64);
-//     context.restore();
-// }
-// img.src = myOrb.imageSRC;
-
-
-// console.log(myOrb);
-// console.log(myOrb2);
-
-// myOrb2.draw(context);
+//test//////////////////////////
+// console.log(svgIdArray); this works
+////////////////////////////////
---
+++ src/svgArray.js
@@ -0,0 +1,14 @@
+export const svgArray = {
+    //need to fill
+};
+
+const svgDiv = document.getElementById("svgs").children;
+export const svgIdArray = [];
+
+for (let svg of svgDiv) {
+        
+     svgIdArray.push(svg.id);
+        
+};
+
+
