6be212c91ae52941c640c75becb6872e34c0f508
Sloont <ColinAbel93@gmail.com>
2021-06-19 09:39:00 -0400

    added files to start making the orbs

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ let context = canvas.getContext("2d");
 var window_height = window.innerHeight;
 var window_width = window.innerWidth;
 
-canvas.width = window_width;
-canvas.height = window_height;
+canvas.width = 500;
+canvas.height = 400;
 
-canvas.style.background = "coral"
+canvas.style.background = "#333"
 
 const convertSVG = (svgid) => {
     const svg = document.getElementById(svgid);
@@ -19,21 +19,41 @@ const convertSVG = (svgid) => {
     const svg64 = btoa(xml);
     const b64Start = 'data:image/svg+xml;base64, ';
 
-    let image64 = b64Start + svg64;
+    return b64Start + svg64;
+}
+
+class Orb {
+    constructor(xpos, ypos, radius, speed, image) {
+        this.xpos = xpos;
+        this.ypos = ypos;
+        this.radius = radius;
+        this.speed = speed;
+        this.image = convertSVG(image);
+    }
+}
+
+const draw = () => {        //for now this is outside of a class
 
     const img  = new Image();
+
     img.onload = function() {
         context.save();
         context.beginPath();
         context.arc(150, 150, 32, 0, Math.PI * 2, false);
-
+    
         context.clip();
         context.drawImage(img, 150-34, 150-34, 68, 68);
         context.restore();
     }
-    img.src = image64;
+    img.src = convertSVG('javascript-icon');
 
 }
 
+const update = () => {
 
-convertSVG('javascript-icon');
+    draw();
+
+
+
+}
+
