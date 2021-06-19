203ce6065b34629a69a77578a54ceba5faeea832
Sloont <colinabel93@gmail.com>
2021-06-19 12:45:46 -0400

    working demo loading images globally

--- src/index.js
+++ src/index.js
@@ -22,53 +22,86 @@ const convertSVG = (svgid) => {
     return b64Start + svg64;
 }
 
+const img1  = new Image();
+img1.src = convertSVG('javascript-icon');
+const img2 = new Image();
+img2.src = convertSVG('java-icon');
+
 class Orb {
     constructor(xpos, ypos, radius, speed, image) {
         this.xpos = xpos;
         this.ypos = ypos;
         this.radius = radius;
         this.speed = speed;
-        this.imageSRC = convertSVG(image);
-        this.image = new Image();
-        
+        this.image = image;
     }
-
-
-    draw(ctx) {
-        let image = this.image;
-        image.onload = function() {
-            // context.save();
-            ctx.beginPath();
-            ctx.arc(this.xpos, this.ypos, this.radius, 0, Math.PI * 2, false);
-            ctx.clip();
-            ctx.drawImage(image, (this.xpos - this.radius), (this.ypos - this.radius), 64, 64);
-            // context.restore();
-            // context.closePath();
-        }
-        image.src = this.imageSRC;
-
-    }
-    update() {
-        this.draw(context);
+    draw(context) {
+            context.save();
+            context.beginPath();
+            context.arc(this.xpos, this.ypos, this.radius, 0, Math.PI * 2, false);
+            context.clip();
+            context.drawImage(this.image, (this.xpos - this.radius), (this.ypos - this.radius), 64, 64);
+            context.restore();
     }
 }
-const myOrb = new Orb(300, 300, 30, 1, 'javascript-icon');
-const myOrb2 = new Orb(150, 150, 30, 1, 'java-icon');
 
-const img  = new Image();
+const myOrb = new Orb(150, 150, 30, 1, img1);
+const myOrb2 = new Orb(350, 150, 30, 1, img2);
 
-img.onload = function() {
-    context.save();
-    context.beginPath();
-    context.arc(myOrb.xpos, myOrb.ypos, myOrb.radius, 0, Math.PI * 2, false);
-    context.clip();
-    context.drawImage(img, (myOrb.xpos-myOrb.radius-2), (myOrb.ypos-myOrb.radius-2), 64, 64);
-    context.restore();
-}
-img.src = myOrb.imageSRC;
-
-
-console.log(myOrb);
-console.log(myOrb2);
-
+window.onload = function() {
+myOrb.draw(context);
 myOrb2.draw(context);
+}
+
+
+
+// class Orb {
+//     constructor(xpos, ypos, radius, speed, image) {
+//         this.xpos = xpos;
+//         this.ypos = ypos;
+//         this.radius = radius;
+//         this.speed = speed;
+//         this.imageSRC = convertSVG(image);
+//         this.image = new Image();
+        
+//     }
+
+
+//     draw(ctx) {
+//         let image = this.image;
+//         image.onload = function() {
+//             // context.save();
+//             ctx.beginPath();
+//             ctx.arc(this.xpos, this.ypos, this.radius, 0, Math.PI * 2, false);
+//             ctx.clip();
+//             ctx.drawImage(image, (this.xpos - this.radius), (this.ypos - this.radius), 64, 64);
+//             // context.restore();
+//             // context.closePath();
+//         }
+//         image.src = this.imageSRC;
+
+//     }
+//     update() {
+//         this.draw(context);
+//     }
+// }
+// const myOrb = new Orb(300, 300, 30, 1, 'javascript-icon');
+// const myOrb2 = new Orb(150, 150, 30, 1, 'java-icon');
+
+// const img  = new Image();
+
+// img.onload = function() {
+//     context.save();
+//     context.beginPath();
+//     context.arc(myOrb.xpos, myOrb.ypos, myOrb.radius, 0, Math.PI * 2, false);
+//     context.clip();
+//     context.drawImage(img, (myOrb.xpos-myOrb.radius-2), (myOrb.ypos-myOrb.radius-2), 64, 64);
+//     context.restore();
+// }
+// img.src = myOrb.imageSRC;
+
+
+// console.log(myOrb);
+// console.log(myOrb2);
+
+// myOrb2.draw(context);
